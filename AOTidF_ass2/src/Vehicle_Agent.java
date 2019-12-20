8769e6dc52cc8f1f34b01479f5e1a1c2ece462e9
Veranika Paulava <veronikapavlova@gmx.de>
2019-12-20 14:37:15 +0100

    create a 80/20 probability for goal save money/save time Next step In ChargingStationRequest add different cases for different battery life

--- AOTidF_ass2/src/Vehicle_Agent.java
+++ AOTidF_ass2/src/Vehicle_Agent.java
@@ -1,10 +1,16 @@
+import java.util.*;
+
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+
+
 
 public class Vehicle_Agent extends Agent{
 	//Battery status of the agent
 	private int battery_life;
 	// Goal to save money "m" or time "t"
-	private String goal;
+	private String goal = RandomGoal();
+	//Position of the agent
 	
 	//Vehicle Initialization
 	protected void setup() {
@@ -15,6 +21,34 @@ public class Vehicle_Agent extends Agent{
 		 * stages of battery life
 		 * 100-80% - agent decide to go to a charging station near 
 		 */
+	}
+	
+	//Generates with 80% probability goal as "save money" (= "m")
+	//and with 20% probability goal to "save time" (= "t")
+	private String RandomGoal() {
+		List<String> list = Collections.nCopies(8, "m");
+		list.addAll(Collections.nCopies(2,"t"));
+		
+		Random rand = new Random();
+		String randomgoal = list.get(rand.nextInt(list.size()));
+		
+		return randomgoal;
+	}
+	
+	private class ChargingStationRequest extends Behaviour{
+
+		@Override
+		public void action() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean done() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		
 	}
 
 }
