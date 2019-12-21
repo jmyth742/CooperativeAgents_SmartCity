16d8faa9c68b25e87e37c077a44d4dd92b518e25
Aristeidis Noulis <anoulis@pop-os.localdomain>
2019-12-21 11:41:12 +0100

    added charging related functions and classes package

--- AOTidF_ass2/src/charging/station/Charging_Station_Agent.java
+++ AOTidF_ass2/src/charging/station/Charging_Station_Agent.java
@@ -1,5 +1,15 @@
+package charging.station;
+
 import jade.core.Agent;
 
 public class Charging_Station_Agent extends Agent{
+	
+	@Override
+	protected void setup() {
+		System.out.println("Hello, let's create an Agent of Charging Station");
+		System.out.println("");
+		//doDelete();
+		
+	}
 
 }
