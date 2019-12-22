129e1c274ec13e5700492b3755e4b2a5b43a3d2e
anoulis <arisnoul@csd.auth.gr>
2019-12-22 20:47:07 +0100

    populate charging event file

--- AOTidF_ass2/src/charging/station/ChargingEvent.java
+++ AOTidF_ass2/src/charging/station/ChargingEvent.java
@@ -1,10 +1,55 @@
 package charging.station;
 
-public class ChargingEvent {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
-	public String getKindOfCharging() {
-		// TODO Auto-generated method stub
-		return "";
+public class ChargingEvent
+{
+    private int id;
+    private static final AtomicInteger idGenerator = new AtomicInteger(0);
+    private final Charging_Station_Agent station;
+    private String kindOfCharging;
+    private long waitingTime;
+    //private ElectricVehicle vehicle;
+    private long chargingTime;
+    private String condition;
+    private Charger charger;
+    private long maxWaitingTime;
+    private long timestamp;
+    private double cost;
+    long accumulatorOfChargingTime = 0;
+    private static final List<ChargingEvent> chargingLog = new ArrayList<>();
+    private static final List<ChargingEvent> exchangeLog = new ArrayList<>();
+
+	
+	public ChargingEvent() {
+        this.id = idGenerator.incrementAndGet();
+    }
+	
+	public void execution() {
+		
 	}
+	
+	 /**
+     * @return The kind of charging of the ChargingEvent.
+     */
+    public String getKindOfCharging()
+    {
+        return kindOfCharging;
+    }
 
-}
+    /**
+     * @return The ChargingStation the ChargingEvent wants to be executed.
+     */
+    public Charging_Station_Agent getStation()
+    {
+    	return station;
+    }
+    
+   }
+
+
+
+
