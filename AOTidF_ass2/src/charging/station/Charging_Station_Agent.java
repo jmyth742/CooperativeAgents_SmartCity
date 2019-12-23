7232bacfe01c94ef00ce742e38d18fc602aeff55
Veranika Paulava <veronikapavlova@gmx.de>
2019-12-23 12:01:35 +0100

    small change

--- AOTidF_ass2/src/charging/station/Charging_Station_Agent.java
+++ AOTidF_ass2/src/charging/station/Charging_Station_Agent.java
@@ -3,7 +3,11 @@ package charging.station;
 import jade.core.Agent;
 
 public class Charging_Station_Agent extends Agent{
-	
+	/**
+	 * Add Yellow Page service here - In book Developing Multi Agent systems with JADE page 
+	 * 72 (or Title 4.4)
+	 * 
+	 */
 	@Override
 	protected void setup() {
 		System.out.println("Hello, let's create an Agent of Charging Station");
